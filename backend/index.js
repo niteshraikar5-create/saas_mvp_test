a42062b0818b3961140c6fd086e8f2de65d8e273
nitesh-raikar <nitesh.raikar@credenceconsultancy.com>
2026-03-02 19:11:11 +0530

    Connect frontent to backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 import express from "express";
+import cors from 'cors';
 const app = express();
 const port  = 5555;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req,res) => {
+app.get('/test', (req,res) => {
     res.send('Hello World!');
+});
+
+app.get('/api/users', (req,res) => {
+    res.json({ message: "Hello from backend" });
 });
 
 app.listen(port, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch('http://localhost:5555/api/users')
+      .then(res => res.json())
+      .then(data => console.log('data',data))
+        
+        // setUsers(data)
+  }, []); // empty array → run once on mount
 
   return (
     <>
